dba7241a2f9af3001b900a1cc7e13ff1b0dd8ef4
Diego Rufino <diegorufino1@gmail.com>
2021-03-11 13:59:07 -0300

    Implementando o CORS

--- app.js
+++ app.js
@@ -9,8 +9,26 @@ const rotaPedidos = require('./routes/pedidos')
 //morgan - monitora todas as acoes mostrando no log
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false })); //apenas dados simples
-app.use(bodyParser.json())
+app.use(bodyParser.json()) //json de entrada no body
 
+//CORS
+app.use((req, res, next) => {
+    // origem (url/site especifico) que ele permite que o acesse
+    res.header('Access-Control-Allow-Origin', '*'); 
+    // quais os cabecalhos que serao aceitos
+    res.header(
+        'Access-Control-Allow-Origin', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    // quais os metodos que poderao ser retornados
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Origin', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});        
+    }
+    next();
+})
+
+// ROTA
 app.use('/produtos', rotaProdutos)
 app.use('/pedidos', rotaPedidos)
 
